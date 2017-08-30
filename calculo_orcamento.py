6b185f5ddca39a1b3e539dd45e83d95e21a54dcd
cs-emerson-batista <emerson.batista@concrete.com.br>
2017-08-30 14:26:22 -0300

    retirado comentarios de referencia

--- calculo_orcamento.py
+++ calculo_orcamento.py
@@ -47,21 +47,3 @@ elif escolha_do_usuario == 'd':
 	print('Seu ticket médio agora é: %.2f' % ticket_medio())
 else:
 	print('sorry, try again: s, n ou d')
-
-
-
-
-# monthrange(2017, 5)[1] - datetime.now().day
-
-# Open the file for reading
-# read_file = open("text.txt", "r")
-
-# Use a second file handler to open the file for writing
-# write_file = open("text.txt", "w")
-# Write to the file
-# # write_file.write("Not closing files is VERY BAD.")
-
-# # Try to read from the file
-# print read_file.read()
-# write_file.close()
-# read_file.close()
